4e3508395877e2ce1e35fad7b5ae5d566457a7fb
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2026-03-04 15:20:26 -0800

    WIP -- add new command line options
    wire up options for sync API URL and token

--- controllers/ingress/reconcile.go
+++ controllers/ingress/reconcile.go
@@ -9,6 +9,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/pomerium/ingress-controller/model"
@@ -32,6 +33,7 @@ func (r *ingressController) reconcileInitial(ctx context.Context) (err error) {
 		return fmt.Errorf("list ingresses: %w", err)
 	}
 
+	originalIngresses := make([]*networkingv1.Ingress, len(ingressList.Items))
 	var ics []*model.IngressConfig
 	for i := range ingressList.Items {
 		ingress := &ingressList.Items[i]
@@ -48,6 +50,7 @@ func (r *ingressController) reconcileInitial(ctx context.Context) (err error) {
 			return fmt.Errorf("fetch ingress %s/%s: %w", ingress.Namespace, ingress.Name, err)
 		}
 		logger.V(1).Info("fetch", "ingress", ingress.Name, "secrets", len(ic.Secrets), "services", len(ic.Services))
+		originalIngresses[i] = ic.Ingress.DeepCopy()
 		ics = append(ics, ic)
 	}
 
@@ -60,6 +63,10 @@ func (r *ingressController) reconcileInitial(ctx context.Context) (err error) {
 			r.IngressNotReconciled(ctx, ingress, fmt.Errorf("update /status: %w", err))
 		} else {
 			r.IngressReconciled(ctx, ingress)
+		}
+		if err := r.Client.Patch(ctx, ingress, client.MergeFrom(originalIngresses[i])); err != nil {
+			// XXX: what to do here?
+			logger.V(1).Info("patch", "ingress", ics[i].Name, "error", err.Error())
 		}
 	}
 
@@ -116,10 +123,19 @@ func (r *ingressController) deleteIngress(ctx context.Context, name types.Namesp
 }
 
 func (r *ingressController) upsertIngress(ctx context.Context, ic *model.IngressConfig) (ctrl.Result, error) {
+	originalIngress := ic.Ingress.DeepCopy()
+
 	_, err := r.IngressReconciler.Upsert(ctx, ic)
 	if err != nil {
 		r.IngressNotReconciled(ctx, ic.Ingress, err)
 		return ctrl.Result{Requeue: true}, fmt.Errorf("upsert: %w", err)
+	}
+
+	// XXX: figure out a better way to do this
+	if err := r.Client.Patch(ctx, ic.Ingress, client.MergeFrom(originalIngress)); err != nil {
+		// XXX: what to do here?
+		logger := log.FromContext(ctx).WithName("upsertIngress")
+		logger.V(1).Info("patch", "ingress", ic.Name, "error", err.Error())
 	}
 
 	r.IngressReconciled(ctx, ic.Ingress)
